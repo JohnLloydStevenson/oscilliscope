1dd56108af058c587534bf0a2c701d647469f411
lmod <john.lloyd.stevenson@gmail.com>
2025-11-10 22:57:41 -0500

    added new project files

---
+++ oscilliscope.srcs/sources_1/new/scope.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/10/2025 10:49:46 PM
+// Design Name: 
+// Module Name: scope
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module scope(
+    output LCD_CS,
+    output LCD_RST,
+    output LCD_DCX,
+    output LCD_WRX,
+    output LCD_RDX,
+    output [15:0] LCD_Data
+    );
+endmodule
