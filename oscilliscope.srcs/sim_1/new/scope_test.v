b7f45739ce81c629da925b7ffe489c005dd6b86b
lmod <john.lloyd.stevenson@gmail.com>
2025-11-13 00:56:27 -0500

    adding delays and commands to initialization sequence

--- oscilliscope.srcs/sim_1/new/scope_test.v
+++ oscilliscope.srcs/sim_1/new/scope_test.v
@@ -1,41 +1,43 @@
 module tb;
-  reg sysclk;
-  reg reset;
+reg sysclk;
+reg reset;
 
-  wire CS;
-  wire RST;
-  wire DCX;
-  wire WRX;
-  wire [15:0] Data;
+	wire CS;
+	wire RST;
+	wire DCX;
+	wire WRX;
+	wire [15:0] Data;
 
-  // instantiate your DUT
-  scope my_scope (
-    .sysclk(sysclk),
-    .reset(reset),
-	.LCD_CS(CS),
-	.LCD_RST(RST),
-	.LCD_DCX(DCX),
-	.LCD_WRX(WRX),
-	.LCD_Data(Data)
-  );
+	// instantiate your DUT
+	scope my_scope (
+			.sysclk(sysclk),
+			.reset(reset),
+			.LCD_CS(CS),
+			.LCD_RST(RST),
+			.LCD_DCX(DCX),
+			.LCD_WRX(WRX),
+			.LCD_Data(Data)
+			);
 
-  // Clock generator (for reference)
-  initial sysclk = 0;
-  always #40 sysclk = ~sysclk;  // 50/4 MHz clock (period = 80 ns)
+	// Clock generator (for reference)
+	initial sysclk = 0;
+	always #40 sysclk = ~sysclk;  // 50/4 MHz clock (period = 80 ns)
 
-  // Stimulus: pulse wr high briefly
-  initial begin
-    #50;               // wait 50 ns
-    reset = 1;
-    #50;               // stay high for 20 ns
-    reset = 0;
-    // stay low forever
-  end
+	// Stimulus: pulse wr high briefly
+	initial begin
+		#50;               // wait 50 ns
+		reset = 1;
+		#50;               // stay high for 50 ns
+		reset = 0;
+		// stay low forever
+	end
 
-  wire [2:0] state;
-  assign state = tb.my_scope.state;
-  wire [5:0] counter;
-  assign counter = tb.my_scope.swreset_counter;
+	wire [3:0] state;
+	assign state = tb.my_scope.state;
+	wire [20:0] counter;
+	assign counter = tb.my_scope.data_counter;
+	wire clk;
+	assign clk = tb.my_scope.lcd_clk;
 
 endmodule
 
--- oscilliscope.srcs/sources_1/new/scope.v
+++ oscilliscope.srcs/sources_1/new/scope.v
@@ -26,7 +26,7 @@ module scope(
     output reg LCD_CS,
     output LCD_RST,
     output reg LCD_DCX,	//1 => data/cmd params, 0 => cmd
-    output LCD_WRX,
+    output reg LCD_WRX,
     output reg [15:0] LCD_Data
     );
 	//local parameters
@@ -34,15 +34,33 @@ module scope(
 	localparam IDLE = 1;
 	localparam SWRESET = 2;
 	localparam SLPOUT = 3;
-	localparam CMD = 4;
-	localparam DATA = 5;
-	localparam NOP = 6;
-	localparam RESET = 7;
+	localparam COLMOD = 4;
+	localparam MADCTL = 5;
+	localparam DISPON = 6;
+	localparam SETCOL = 7;
+	localparam SETROW = 8;
+	localparam CMD = 9;
+	localparam DATA = 10;
+	localparam NOP = 11;
+	localparam RESET = 12;
 
-	localparam CMD_NOP = 16'h0000;
-	localparam CMD_SWRESET = 16'h0001;
-	localparam CMD_SLPOUT = 16'h0011;
-	localparam CMD_MEMWRITE = 16'h002c;
+	localparam CMD_NOP = 16'h00;
+	localparam CMD_SWRESET = 16'h01;
+	localparam CMD_SLPOUT = 16'h11;
+	localparam CMD_COLMOD = 16'h3a;
+	localparam CMD_MADCTL = 16'h36;
+	localparam CMD_DISPON = 16'h29;
+	localparam CMD_SETCOL = 16'h2a;
+	localparam CMD_SETROW = 16'h2b;
+	localparam CMD_MEMWRITE = 16'h2c;
+
+	localparam DATA_COLMOD = 16'h55;	//16-bit RGB565
+	localparam DATA_MADCTL = 16'h48;	//for RGB order
+	localparam [63:0] DATA_SETCOL = {16'h0, 16'h0, 16'h1, 16'he0};	//0-480
+	localparam [63:0] DATA_SETROW = {16'h0, 16'h0, 16'h1, 16'h40};	//0-320
+
+	localparam PIXEL_COUNT = 480*320;
+
 
 	//clocks
 	reg [8:0] clk;
@@ -51,23 +69,28 @@ module scope(
 		clk <= clk+1;
 	end
 	wire lcd_clk;
-	assign lcd_clk = clk[6];
+	assign lcd_clk = clk[3];
+
 
 	//mealy msf state register
 	reg [3:0] state;
 	reg [3:0] next_state;
-	reg [5:0] boot_counter;
-	reg [5:0] swreset_counter;
-	reg [5:0] slpout_counter;
+	reg [18:0] boot_counter;
+	reg [15:0] swreset_counter;
+	reg [18:0] slpout_counter;
+	reg [15:0] dispon_counter;
 	reg [17:0] data_counter;
+	reg [20:0] state_counter;	//1 counter to rule them all
 	initial begin
 		state = BOOT;
 		next_state = BOOT;
-		boot_counter = 6'b0;
-		swreset_counter = 6'b0;
-		slpout_counter = 6'b0;
+		boot_counter = 19'b0;
+		swreset_counter = 16'b0;
+		slpout_counter = 19'b0;
 		data_counter = 18'b0;
+		state_counter = 21'b0;
 	end
+
 
 	//msf sequential block
 	always @(posedge lcd_clk) begin
@@ -81,17 +104,17 @@ module scope(
 	always @(*) begin
 		case (state)
 			BOOT:
-				if (boot_counter == {6{1'b1}})
+				if (boot_counter == {17{1'b1}})
 					next_state = SWRESET;
 				else
 					next_state = BOOT;
 			SWRESET:
-				if (swreset_counter == {6{1'b1}})
+				if (swreset_counter == {14{1'b1}})
 					next_state = SLPOUT;
 				else
 					next_state = SWRESET;
 			SLPOUT:
-				if (slpout_counter == {6{1'b1}})
+				if (slpout_counter == {17{1'b1}})
 					next_state = IDLE;
 				else
 					next_state = SLPOUT;
@@ -100,7 +123,7 @@ module scope(
 			CMD:
 				next_state = DATA;
 			DATA:
-				if (data_counter < 480*320)
+				if (data_counter < PIXEL_COUNT)
 					next_state = DATA;
 				else
 					next_state = NOP;
@@ -112,6 +135,7 @@ module scope(
 				next_state = IDLE;
 		endcase
 	end
+
 
 	//substate counters
 	always @(posedge lcd_clk) begin
@@ -140,24 +164,38 @@ module scope(
 			slpout_counter <= 0;
 	end
 
+
 	//lcd signals
 	always @(*) begin
 		case (state)
-			BOOT:
-				LCD_CS = 1;
-			IDLE:
-				LCD_CS = 1;
 			SWRESET:
-				LCD_CS = 1;
-			default:
+				LCD_CS = swreset_counter < 2 ? 0 : 1;
+			SLPOUT:
+				LCD_CS = slpout_counter < 2 ? 0 : 1;
+			CMD:
 				LCD_CS = 0;
+			DATA:
+				LCD_CS = data_counter < PIXEL_COUNT ? 0 : 1;
+			default:
+				LCD_CS = 1;
 		endcase
 	end
+
 	always @(*) begin
 		case (state)
 			SWRESET:
 				LCD_DCX = 0;
 			SLPOUT:
+				LCD_DCX = 0;
+			COLMOD:
+				LCD_DCX = 0;
+			MADCTL:
+				LCD_DCX = 0;
+			DISPON:
+				LCD_DCX = 0;
+			SETCOL:
+				LCD_DCX = 0;
+			SETROW:
 				LCD_DCX = 0;
 			CMD:
 				LCD_DCX = 0;
@@ -167,8 +205,35 @@ module scope(
 				LCD_DCX = 1;
 		endcase
 	end
-	assign LCD_WRX = !LCD_CS && !lcd_clk;
-	assign LCD_RST = !(state == BOOT && boot_counter < 6'b010000);
+
+	always @(*) begin
+		case (state)
+			SWRESET:
+				LCD_WRX = swreset_counter < 1 ? !lcd_clk : 1;
+			SLPOUT:
+				LCD_WRX = slpout_counter < 1 ? !lcd_clk : 1;
+			COLMOD:
+				LCD_WRX = !lcd_clk;
+			MADCTL:
+				LCD_WRX = !lcd_clk;
+			DISPON:
+				LCD_WRX = dispon_counter < 1 ? !lcd_clk : 1;
+			SETCOL:
+				LCD_WRX = !lcd_clk;
+			SETROW:
+				LCD_WRX = !lcd_clk;
+			CMD:
+				LCD_WRX = !lcd_clk;
+			DATA:
+				LCD_WRX = !lcd_clk;
+			NOP:
+				LCD_WRX = !lcd_clk;
+			default:
+				LCD_WRX = 1;
+		endcase
+	end
+
+	assign LCD_RST = !(state == BOOT && boot_counter < 19'b100000000000000);
 
 	always @(*) begin
 		case (state)
